3adf184ec974bd42c52effa1792895d84ff0a033
Zach Davis <zacharydavis92@gmail.com>
2022-07-19 15:46:55 -0500

    'small ui changes'

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -71,21 +71,21 @@ const Dice = () => {
     }, []);
 
     return (
-        <div className="absolute w-screen h-1/3 flex flex-col items-center text-center justify-between top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-            <div className="flex flex-row w-11/12 justify-between mb-2">
+        <div className="absolute w-screen h-1/2 flex flex-col items-center text-center justify-between top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+            <div className="flex flex-row w-11/12 justify-between mt-4">
                 <button id="easyBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={easySelected}>Easy</button>
                 <button id="medBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={mediumSelected}>Medium</button>
                 <button id="hardBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={hardSelected}>Hard</button>
             </div>
             <div className="flex flex-col w-screen items-center">
-                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 m-2 w-3/4">
+                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mb-4 w-3/4">
                     <p className="text-2xl">{results[0]}</p>
                 </div>
-                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 m-2 w-3/4">
+                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mt-2 w-3/4">
                     <p className="text-2xl">{results[1]}</p>
                 </div>
             </div>
-            <button className="bg-sky-600 border-black border-2 rounded-lg p-2 w-1/2 text-2xl font-semibold sm:mt-4" onClick={rollDice}>Roll!</button>
+            <button className="bg-sky-600 border-black border-2 rounded-lg p-2 w-1/2 text-2xl font-semibold sm:mt-4 active:bg-sky-500 active:border-yellow-300" onClick={rollDice}>Roll!</button>
         </div>
     )
 }
