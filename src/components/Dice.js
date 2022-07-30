379a6f9a81efb9515f36f369bb241705df612f0f
Zach Davis <zacharydavis92@gmail.com>
2022-07-30 17:27:07 -0500

    'added modal'

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,9 +1,10 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import { createEasyArrays, createMediumArrays, createHardArrays, roll } from "../utils/logic";
 
 const Dice = () => {
     const [arraysToUse, setArraysToUse] = useState();
     const [results, setResults] = useState(["Disc Type", "Disc Shot"]);
+    const [showModal, setShowModal] = useState(true);
 
     const easySelected = () => {
         let easyButton = document.getElementById('easyBtn');
@@ -22,6 +23,7 @@ const Dice = () => {
         hardButton.classList.add("border-black");
         hardButton.classList.add("bg-sky-600");
         setArraysToUse(createEasyArrays());
+        toggleModal();
     };
 
     const mediumSelected = () => {
@@ -41,6 +43,7 @@ const Dice = () => {
         hardButton.classList.add("border-black");
         hardButton.classList.add("bg-sky-600");
         setArraysToUse(createMediumArrays());
+        toggleModal();
     };
 
     const hardSelected = () => {
@@ -60,33 +63,47 @@ const Dice = () => {
         hardButton.classList.remove("border-black");
         hardButton.classList.remove("bg-sky-600");
         setArraysToUse(createHardArrays());
+        toggleModal();
     };
+
+    const toggleModal = () => {
+        setShowModal(false);
+    }
 
     const rollDice = () => {
         setResults(roll(arraysToUse));
     };
 
-    useEffect(() => {
-        mediumSelected();
-    }, []);
-
     return (
-        <div className="absolute w-screen h-1/2 flex flex-col items-center text-center justify-between top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-            <div className="flex flex-row w-11/12 justify-between mt-4">
-                <button id="easyBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={easySelected}>Easy</button>
-                <button id="medBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={mediumSelected}>Medium</button>
-                <button id="hardBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={hardSelected}>Hard</button>
-            </div>
-            <div className="flex flex-col w-screen items-center">
-                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mb-4 w-3/4">
-                    <p className="text-2xl">{results[0]}</p>
+        <>
+            {showModal && <div>
+                <div className="bg-black opacity-80 h-screen w-screen absolute z-10"></div>
+                <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-20 bg-sky-600 border-2 rounded-lg border-black h-fit w-3/4 flex flex-col justify-center text-center">
+                    <h1 className="text-4xl m-4">Select a Difficulty</h1>
+                    <div className="flex flex-col justify-between items-center m-2">
+                        <button className="bg-sky-400 border-yellow-400 border-2 rounded-lg w-1/2 m-2 p-1 text-2xl font-semibold" onClick={easySelected}>Easy</button>
+                        <button className="bg-sky-400 border-yellow-400 border-2 rounded-lg w-1/2 m-2 p-1 text-2xl font-semibold" onClick={mediumSelected}>Medium</button>
+                        <button className="bg-sky-400 border-yellow-400 border-2 rounded-lg w-1/2 m-2 p-1 text-2xl font-semibold" onClick={hardSelected}>Hard</button>
+                    </div>
                 </div>
-                <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mt-2 w-3/4">
-                    <p className="text-2xl">{results[1]}</p>
+            </div>}
+            <div className="absolute w-screen h-1/2 flex flex-col items-center text-center justify-between top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+                <div className="flex flex-row w-11/12 justify-between mt-4">
+                    <button id="easyBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={easySelected}>Easy</button>
+                    <button id="medBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={mediumSelected}>Medium</button>
+                    <button id="hardBtn" className="bg-sky-600 border-black border-2 rounded-lg w-1/3 m-2 p-1 text-2xl font-semibold sm:mt-4" onClick={hardSelected}>Hard</button>
                 </div>
+                <div className="flex flex-col w-screen items-center">
+                    <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mb-4 w-3/4">
+                        <p className="text-2xl">{results[0]}</p>
+                    </div>
+                    <div className="bg-slate-300 border-black border-2 rounded-lg p-2 mt-2 w-3/4">
+                        <p className="text-2xl">{results[1]}</p>
+                    </div>
+                </div>
+                <button className="bg-sky-600 border-black border-2 rounded-lg p-2 w-1/2 text-2xl font-semibold sm:mt-4 active:bg-sky-500 active:border-yellow-300" onClick={rollDice}>Roll!</button>
             </div>
-            <button className="bg-sky-600 border-black border-2 rounded-lg p-2 w-1/2 text-2xl font-semibold sm:mt-4 active:bg-sky-500 active:border-yellow-300" onClick={rollDice}>Roll!</button>
-        </div>
+        </>
     )
 }
 
